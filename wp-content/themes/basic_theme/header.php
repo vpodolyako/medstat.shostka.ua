2c590fa4728ec795f755c57c01f08b5c6376f714
vpodolyako <vpodolyako@gmail.com>
2020-04-16 16:05:55 +0300

    add_statia_1

--- wp-content/themes/basic_theme/header.php
+++ wp-content/themes/basic_theme/header.php
@@ -5,13 +5,33 @@
 
 <head>
     <meta charset="UTF-8">
+    <title>Відділ аналізу медичної статистики Шосткинського району</title>
+    <link href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <?php wp_head(); ?>
 </head>
 
 <body>
-    <div class="body-wrap" id="body-wrap">
+ 
+        <div class="header">
+            <h1>Відділ аналізу медичної статистики Шосткинського району</h1>
 
-        <header class="header"  id="header">
-            <h1>Header</h1>
-        </header>
+        <div class="contact">
+            <h2>&#9742;(05449) 76 4 35</h2>
+            <h2>&#9993;shostkarcpmsd2018@ukr.net</h2>
+         </div>
+         <div class="head_menu">
+         <ul>
+              <li><a href="#">Головна</a></li>
+              <li><a href="#">Новини</a></li>
+              <li><a href="#">Аналіз роботи</a></li>
+              <li><a href="#">Документи</a></li>
+              <li><a href="#">Мережа</a></li>
+              <li><a href="#">Контакти</a></li>
+        </ul>
+        </div>
+        
+
+
+</body>
+</html>
