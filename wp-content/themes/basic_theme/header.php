b5045be13d9c22eb0603de77b62f1593bc7ee046
vpodolyako <vpodolyako@gmail.com>
2020-04-17 14:00:42 +0300

    add_glavnaya

--- wp-content/themes/basic_theme/header.php
+++ wp-content/themes/basic_theme/header.php
@@ -9,23 +9,95 @@
     <link href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <?php wp_head(); ?>
-    
+    <style>
+       .logo img {
+           
+           border: 1px solid indigo;
+           max-width: 100%;
+           height: auto;
+           float: left;
+           margin: 5px;
+           vertical-align:bottom;
+           
+           
+       } 
+       .nazva h1 {
+           font-size: 50px;
+           color: #600473;
+           font-weight: bold;
+           border: 1px solid indigo;
+           width: 100%;
+           height: 50px;
+           position: relative;
+           left: 70px;
+           top: 20px;
+           display: inline-block;
+           float: left;
+           margin: 0 auto;
+           
+       }
+       .phone, .email {
+           display: inline;
+           font-size: 20px;
+           color: black;
+           border: 1px solid indigo;
+           width: 100%;
+           height: 50px;
+           float: left;
+           clear: left;
+          
+       } 
+       
+       .head_menu {
+           list-style: none;
+           margin: 0;
+           padding-left: 0;
+           
+           
+       }
+       .head_menu li {
+           display: inline;
+           margin-right: -5px;
+           border: 1px solid indigo;
+           width: 150px;
+           height: 40px;
+           top:50px;
+           
+           
+           
+       }
+       .head_menu a {
+           font-family: sans-serif;
+           text-decoration: none;
+           color: #5757a0;
+           font-weight: bold;
+           display: inline-block;
+           padding: 10px 20px;
+           background-color: lavender;
+           border-bottom: 5px solid #600473;
+       }
+       
+         
+    </style>
 
 </head>
 
 <body>
-    <div class="logo">
-        <img src="img/Penguins.jpeg"> 
- 
-        <div class="header">
-            <h1>Відділ аналізу медичної статистики Шосткинського району</h1>
 
-        <div class="contact">
-            <h2>&#9742;(05449) 76 4 35</h2>
-            <h2>&#9993;shostkarcpmsd2018@ukr.net</h2>
-         </div>
-         <div class="head_menu">
-         <ul>
+        <div class="header">
+
+        <div class="logo">
+             <img src="<?php bloginfo('template_directory');?>/img/emblema.jpg"> 
+        </div>
+        <span class="nazva"> 
+            <h1>Відділ аналізу медичної статистики Шосткинського району</h1>
+        </span>
+        <div class="phone">&#128222;(05449) 76 4 35</div>
+        <div class="email">&#9993;shostkarcpmsd2018@ukr.net</div>
+
+         
+         <ul class="head_menu">
+         
               <li><a href="#">Головна</a></li>
               <li><a href="#">Новини</a></li>
               <li><a href="#">Аналіз роботи</a></li>
@@ -34,6 +106,7 @@
               <li><a href="#">Контакти</a></li>
         </ul>
         </div>
+    
         
 
 
