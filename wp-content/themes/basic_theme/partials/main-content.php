b3289e15cae4bc3e2d5ebff390eb0d72f33d76a3
vpodolyako <vpodolyako@gmail.com>
2020-05-12 21:24:35 +0300

    add_diagrama

--- wp-content/themes/basic_theme/partials/main-content.php
+++ wp-content/themes/basic_theme/partials/main-content.php
@@ -2,9 +2,9 @@
 
 <div class="chart_body">
     <div class="owl-carousel">
-        <div> <canvas id="levelChart" width="300" height="100"></canvas></div>
-        <div> <canvas id="levelChart1" width="300" height="100"></canvas> </div>
-        <div> <canvas id="levelChart2" width="300" height="100"></canvas></div>
+        <div> <canvas id="levelChart" width="200" height="50"></canvas></div>
+        <div> <canvas id="levelChart1" width="200" height="50"></canvas> </div>
+        
     </div>
     
 </div>
--- wp-content/themes/basic_theme/scripts/main.js
+++ wp-content/themes/basic_theme/scripts/main.js
@@ -9,8 +9,61 @@ jQuery( document ).ready(function() {
     Chart.defaults.global.defaultFontSize = 16;
     
     var levelData = {
+      label: 'СТРУКТУРА СЕРЦЕВО-СУДИННИХ ХВОРОБ ЗА 2019 РІК',
+      data: [3090, 871, 15, 913, 37],
+      backgroundColor: [
+        'rgba(0, 99, 132, 0.6)',
+        'rgba(30, 99, 132, 0.6)',
+        'rgba(60, 99, 132, 0.6)',
+        'rgba(90, 99, 132, 0.6)',
+        'rgba(120, 99, 132, 0.6)',
+        'rgba(150, 99, 132, 0.6)',
+        'rgba(180, 99, 132, 0.6)',
+        'rgba(210, 99, 132, 0.6)',
+        'rgba(240, 99, 132, 0.6)'
+      ],
+      borderColor: [
+        'rgba(0, 99, 132, 1)',
+        'rgba(30, 99, 132, 1)',
+        'rgba(60, 99, 132, 1)',
+        'rgba(90, 99, 132, 1)',
+        'rgba(120, 99, 132, 1)',
+        'rgba(150, 99, 132, 1)',
+        'rgba(180, 99, 132, 1)',
+        'rgba(210, 99, 132, 1)',
+        'rgba(240, 99, 132, 1)'
+      ],
+      borderWidth: 1.5,
+      hoverBorderWidth: 0
+    };
+    
+    var chartOptions = {
+      scales: {
+        yAxes: [{
+          barPercentage: 0.5
+        }]
+      },
+      elements: {
+        rectangle: {
+          borderSkipped: 'left',
+        }
+      }
+    };
+
+    var barChart = new Chart(levelCanvas2, {
+      type: 'bar',
+      data: {
+        labels: ["Ішемічна хвороба серця", "Стенокардія", "Інфаркт міокарда", "Гіпертонічна хвороба", "Інсульт"],
+        datasets: [levelData],
+      },
+      options: chartOptions
+    });
+
+
+
+    var levelData = {
       label: 'РІВЕНЬ ЗАХВОРЮВАНОСТІ ЗА 2019 РІК',
-      data: [5427, 5243, 5514, 3933, 1326, 687, 1271],
+      data: [2815.7, 1519.4, 998.8, 653.5, 387.8, 595.0, 393.1],
       backgroundColor: [
         'rgba(0, 99, 132, 0.6)',
         'rgba(30, 99, 132, 0.6)',
@@ -50,25 +103,7 @@ jQuery( document ).ready(function() {
       }
     };
     
-    var barChart = new Chart(levelCanvas2, {
-      type: 'horizontalBar',
-      data: {
-        labels: ["Хв. системи кровообігу", "Хв. сечостатевої системи", "Хв.органів травлення", "Хв. кістково-м'язової с-ми", "Новоутворення", "Хв. шкіри", "Хв. ендокринної с-ми"],
-        datasets: [levelData],
-      },
-      options: chartOptions
-    });
-
-    var barChart2 = new Chart(levelCanvas3, {
-      type: 'horizontalBar',
-      data: {
-        labels: ["Хв. системи кровообігу", "Хв. сечостатевої системи", "Хв.органів травлення", "Хв. кістково-м'язової с-ми", "Новоутворення", "Хв. шкіри", "Хв. ендокринної с-ми"],
-        datasets: [levelData],
-      },
-      options: chartOptions
-    });
-
-    var barChart3 = new Chart(levelCanvas, {
+    var barChart2 = new Chart(levelCanvas, {
       type: 'horizontalBar',
       data: {
         labels: ["Хв. системи кровообігу", "Хв. сечостатевої системи", "Хв.органів травлення", "Хв. кістково-м'язової с-ми", "Новоутворення", "Хв. шкіри", "Хв. ендокринної с-ми"],
